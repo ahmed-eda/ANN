e5c5a12e36da7aee77ff0ffddf214b3aeb415310
ahmed-expert <ahmed_expert@hotmail.com>
2023-04-16 01:14:40 +0200

    test154

---
+++ testLev_all_Pion_n_3_7_14_28_28_14_7_1_epoch_160_activation_changed.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+
+# Read the data from the CSV file
+data = pd.read_csv('data_pion.csv')
+
+# Split the data into input and output variables
+X = data.drop('output', axis=1)
+y = data['output']
+
+# Define the model
+model = Sequential()
+model.add(Dense(7, input_dim=X.shape[1], activation='linear'))
+model.add(Dense(14, activation='linear'))
+model.add(Dense(28, activation='linear'))
+model.add(Dense(28, activation='linear'))
+model.add(Dense(14, activation='linear'))
+model.add(Dense(7, activation='linear'))
+model.add(Dense(1, activation='linear'))
+#linear
+# Compile the model with Levenberg-Marquardt optimizer
+optimizer = RMSprop(lr=0.001, rho=0.001, epsilon=None, decay=0.0)
+model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+# Train the model
+model.fit(X, y, epochs=160, batch_size=3, validation_split=0.2)
+
+# Make predictions on new data
+new_data = pd.read_csv('data_pion.csv').drop('output', axis=1)
+#predictions = model.predict(new_data)
+print("new_data is : ")
+print(new_data)
+predictions = model.predict(new_data)
+print("predictions is : ")
+print(predictions)
+
+# Write predictions to CSV file
+output = pd.DataFrame(predictions, columns=['predictions'])
+output.to_csv('predictions.csv', index=False)
+
+print("End")
--- testLev_all_Pion_n_3_7_5_1_epoch_160.py
+++ testLev_all_Pion_n_3_7_5_1_epoch_160.py
@@ -1,38 +1,39 @@
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import RMSprop
 
 # Read the data from the CSV file
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data_pion.csv')
 
 # Split the data into input and output variables
 X = data.drop('output', axis=1)
 y = data['output']
 
-input_test=X.shape[1]
-print(" input_test")
-print(input_test)
-print("stat network")
 # Define the model
 model = Sequential()
 model.add(Dense(7, input_dim=X.shape[1], activation='relu'))
 model.add(Dense(5, activation='relu'))
-#model.add(Dense(16, activation='relu'))
 model.add(Dense(1, activation='linear'))
-#model.add(Dense(1, activation='sigmoid'))
 
-# Compile the model
-#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.compile(loss='mean_squared_error', optimizer='adam')
+# Compile the model with Levenberg-Marquardt optimizer
+optimizer = RMSprop(lr=0.01, rho=0.001, epsilon=None, decay=0.0)
+model.compile(loss='mean_squared_error', optimizer=optimizer)
 
 # Train the model
-model.fit(X, y, epochs=72, batch_size=32, validation_split=0.2)
+model.fit(X, y, epochs=160, batch_size=32, validation_split=0.2)
 
 # Make predictions on new data
-new_data = pd.read_csv('data_20.csv').drop('output', axis=1)
+new_data = pd.read_csv('data_pion.csv').drop('output', axis=1)
+#predictions = model.predict(new_data)
 print("new_data is : ")
 print(new_data)
 predictions = model.predict(new_data)
 print("predictions is : ")
 print(predictions)
+
+# Write predictions to CSV file
+output = pd.DataFrame(predictions, columns=['predictions'])
+output.to_csv('predictions.csv', index=False)
+
 print("End")
