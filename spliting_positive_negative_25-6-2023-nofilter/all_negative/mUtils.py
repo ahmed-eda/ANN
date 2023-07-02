1739f99fb628f31b019f7c96ec3797e4a628dd0c
ahmed-expert <ahmed_expert@hotmail.com>
2023-07-02 19:25:19 +0300

    123 3

--- spliting_positive_negative_25-6-2023-nofilter/all_negative/mUtils.py
+++ spliting_positive_negative_25-6-2023-nofilter/all_negative/mUtils.py
@@ -12,53 +12,111 @@ from sklearn.metrics import mean_squared_error
 from io import StringIO
 
 
-def get_input_file_name():
-    excel_files = [
-        f for f in os.listdir(".") if f.endswith(".xlsx") and not f.startswith("out")
-    ]
-    if not excel_files:
-        raise ValueError("No Excel files found in the current directory")
-    return excel_files[0]
-
-
 class MyParam:
-    inputFile = None
-    inputSheetName = None
-    data = None
-    X = None
-    y = None
+    def __init__(self):
+        # data parameters
+        self.inputFile = ''
+        self.inputSheetName = ''
+        self.data = ''
+        self.X = ''
+        self.y = ''
+        self.X_train = ''  
+        # Model parameteres  
+        self.myepochs = 0
+        self.mybatchSize = 0
+        self.modelName = ''
+        # Output parameters
+        self.outputFile = ''
+        self.summaryOutFile = ''
+        self.outputSheetName = ''
+        self.nameFigImg = ''
+        pass
 
-def init_param(p):
-    p.inputFile = get_input_file_name()
-    p.inputSheetName = "Sheet1"
-    p.data = None
-    p.X = None
-    p.y = None
-    return p
+# utility function
 
+    # Get current data excel file name (doesn't start with out* ) 
+    def get_input_file_name():
+        excel_files = [
+            f for f in os.listdir(".") if f.endswith(".xlsx") and not f.startswith("out")
+        ]
+        if not excel_files:
+            raise ValueError("No Excel files found in the current directory")
+        return excel_files[0]
 
-def get_data_in_Param(p):
-    # Read the data from the excel file in class of parameters
+    # get model file name
+    def get_model_fromfile():
+        modelNames = [f for f in os.listdir('.') if (f.endswith('.h5'))]
+        if not modelNames:
+            raise ValueError("No Excel files found in the current directory")
+        return modelNames[0]
+
+# Methods
+
+# init
+    def init_param(self,_inputfile='',sheetName='Sheet1'):
+        # data parameters
+        if(len(_inputfile)>0):
+            self.inputFile = _inputfile
+        else:
+            self.inputFile = self.get_input_file_name() # get current excel file in the folder
+        
+        self.inputSheetName = "Sheet1"
+        self.data = ''
+        self.X = ''
+        self.y = ''
+        self.X_train = ''  
+        # Model parameteres  
+        self.myepochs = int(100) 
+        self.mybatchSize = int(16)
+        self.modelName = ''
+        # Output parameters
+        self.outputFile = ''
+        self.summaryOutFile = ''
+        self.outputSheetName = ''
+        self.nameFigImg = ''
     
-    data_all = pd.read_excel(p.inputFile, sheet_name=p.inputSheetName)
-    temp_data_all = data_all  # data_all[data_all['spectrum']<60]
-    data = temp_data_all.reset_index(drop=True)
-    # Split the data into input and output variables
-    X = data[["mass", "s", "N part", "Pt"]]
-    y = data["spectrum"].to_frame("spectrum")
-    p.data = data
-    p.X = X
-    p.y = y
-    print(p.X.head)
-    print(p.y.head)
-    return p
+# read data
+    def get_data_in_Param(self):
+        # Read the data from the excel file in class of parameters
+        data_all = pd.read_excel(p.inputFile, sheet_name=p.inputSheetName)
+        temp_data_all = data_all  # data_all[data_all['spectrum']<60]
+        data = temp_data_all.reset_index(drop=True)
+        # Split the data into input and output variables
+        self.X = data[["mass", "s", "N part", "Pt"]]
+        self.y = data["spectrum"].to_frame("spectrum")
+        self.data = data
+        print(self.X.head)
+        print(self.y.head)
+        
+# normaliz input
+    def normaliz_data(self):
+        # Normalize the input
+        from sklearn.preprocessing import RobustScaler
+        # Create a RobustScaler object
+        scaler = RobustScaler()
+        # Fit the scaler to the input data and transform it
+        X_normalized = scaler.fit_transform(self.X)
+        # Print the normalized input data
+        print('X_normalized')
+        print(X_normalized)
+        self.X_train = X_normalized
+        
+ # set model and config param
+    def set_model_config_param(self,_modelName=''):
+        if(len(_modelName)>0):
+            self.modelName=_modelName
+        else:
+            self.modelName = self.get_model_fromfile()
+        
+        if len(self.modelName)>0:
+            raise ValueError("No model files found in the current directory")
+        
+        # Output parameters
+        self.outputFile = 'out_ '+self.modelName+' .xlsx'
+        self.summaryOutFile = self.modelName + ' _ Summary .txt'
+        self.outputSheetName = 'predicat_ '+self.modelName+' '
+        self.nameFigImg = 'fig_in4_ '+self.modelName+' .png'
 
+ # end       
+    
 
-""" def InitParam():
-    # return class of Param
-    # call the parameters
-    myInitParam = MyParam()
-
-    # return it
-    return myInitParam
- """
--- spliting_positive_negative_25-6-2023-nofilter/all_negative/main.py
+++ spliting_positive_negative_25-6-2023-nofilter/all_negative/main.py
@@ -1,14 +1,32 @@
 import mUtils as u
 
+def our_procedure(_fileName='',_modelName = ''):
+    # return parameter class contain only structur to use in app
+    p = u.MyParam() 
+    # give the file name to class parameters , also Sheet name if don't default is 'Sheet1'
+    p.init_param() # get current excel sheet (only one in folder)               
+    ''' get the data from excel file and save it class parameter
+        X value = X = data[["mass", "s", "N part", "Pt"]]
+        y value = y = data["spectrum"]
+    '''
+    p.get_data_in_Param()
+    # normaliz input
+    p.normaliz_data()
+
+    # give the model name to class parameters 
+    p.set_model_config_param() 
+
+
+
+    # Then we will start at normalizing data
+    return True
+
+###################################################################
+
 # start the main function
 def main():
     print("Starting main call")
-    p = u.MyParam()
-    p = u.init_param(p)
-    p = u.get_data_in_Param(p)
-    print("p.y : ", p.y)
-
-    # Then we will start at normalizing data
+    our_procedure()    
     print("The End")
 
 
